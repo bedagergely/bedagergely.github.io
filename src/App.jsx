df91f97e4782a464d54925c19c1bbb20cf35274d
Gergely Beda <gergely.beda@transferwise.com>
2023-01-03 22:32:07 +0100

    First day

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,60 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [arrayList, setArrayList] = useState([[]]);
+  const [result, setResult] = useState('');
+
+  function addArray(){
+    setArrayList([...arrayList, []]);
+  }
+
+  function popArray(){
+    let x = arrayList.pop();
+    if(arrayList.length == 0) return;
+    setArrayList([...arrayList]);
+  }
+
+  function generateResult(){
+    setResult('Hello World');
+  }
+
+  function setFieldValue(index, value){
+    arrayList[index-1] = value;
+    setArrayList([...arrayList])
+    console.log(arrayList[index-1]);
+  }
 
   return (
     <div className="App">
-      <ArrayField></ArrayField>
+      <ArrayBlock arrayList={arrayList} addArray={addArray} popArray={popArray} setFieldValue={setFieldValue}></ArrayBlock>
       <TemplateField></TemplateField>
-      <Result></Result>
-      <GenerateResultButton></GenerateResultButton>
+      <Result value={result}></Result>
+      <GenerateResultButton generateResult={generateResult}></GenerateResultButton>
     </div>
   )
 }
 
-function ArrayField() {
+function ArrayField(props) {
+  function handleChange(event){
+    props.setFieldValue(props.index, event.target.value);
+  }
+
   return (
     <div className='ArrayField'>
-      <textarea></textarea>
+        <textarea onChange={handleChange}></textarea>
+        <p>{props.index}</p>
+    </div>
+  )
+}
+
+function ArrayBlock(props){
+  return (
+    <div className='ArrayBlock'>
+      {props.arrayList.map((arr, index) => {
+        return <ArrayField key={index} index={index + 1} setFieldValue={props.setFieldValue}></ArrayField>;
+      })}
+      <button onClick={props.addArray}>+</button>
+      <button onClick={props.popArray}>-</button>
     </div>
   )
 }
@@ -30,24 +68,20 @@ function TemplateField(){
   )
 }
 
-function Result(){
+function Result(props){
   return (
     <div className='Result'>
-
+      <p>{props.value}</p>
     </div>
   )
 }
 
-function GenerateResultButton(){
+function GenerateResultButton(props){
   return (
     <div className='GenerateResultButton'>
-      <button onClick={generateResult}>Generate</button>
+      <button onClick={props.generateResult}>Generate</button>
     </div>
   )
-}
-
-function generateResult(){
-  console.log("Hello world");
 }
 
 export default App
